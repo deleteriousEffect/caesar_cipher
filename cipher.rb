bf27684987f6a0e64fb1c8ec4b08efac329687f7
Hayley Swimelar <hswimelar@gmail.com>
2015-02-23 15:22:24 -0800

    add basic cyphering

--- cipher.rb
+++ cipher.rb
@@ -1,2 +1,13 @@
 def caesar_cipher(message, shift)
+  characters = message.split(//)
+  encoded_message = []
+
+  characters.each do |char|
+    shifted_char = char.ord + shift
+    encoded_message << shifted_char.chr
+  end
+  encoded_message.join
 end
+
+puts caesar_cipher('bat', 1)
+puts caesar_cipher('bat', -1)
